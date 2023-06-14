35e73d19629f34eb41fc8eb35b50764652b00756
sum28it <prasad28sumit@gmail.com>
2023-06-14 08:13:53 +0530

    Remove redundant variable pi

--- Chapter-1/1.12/main.go
+++ Chapter-1/1.12/main.go
@@ -55,14 +55,13 @@ func lissajous(out io.Writer, cycles int) {
 		nframes = 64    // number of animation frames
 		delay   = 8     // delay between frames in 10ms units
 	)
-	pi := math.Pi
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // phase difference
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < float64(cycles)*2*pi; t += res {
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
